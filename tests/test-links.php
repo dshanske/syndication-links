c48a7b9752e34eb6c55ecda3951a0b95f4c40a4b
David Shanske <david@shanske.com>
2018-06-26 00:50:53 -0500

    Add syndication link tests

---
+++ tests/test-links.php
@@ -0,0 +1,37 @@
+<?php
+class SLinksTest extends WP_UnitTestCase {
+	 
+	public function test_set_and_retrieve_syndication_link() {
+		$post_id = self::factory()->post->create();
+		$link = 'http://www.example.com';
+		add_syndication_link( $post_id, $link );
+		$get = get_syndication_links_data( $post_id );
+		$this->assertEquals( array( $link ), $get );
+	}
+
+	public function test_set_and_retrieve_syndication_links() {
+		$post_id = self::factory()->post->create();
+		$link = array( 'http://www.example.com', 'http://www.example2.com' );
+		add_syndication_link( $post_id, $link );
+		$get = get_syndication_links_data( $post_id );
+		$this->assertEquals( $link, $get );
+	}
+
+	public function test_add_two_syndication_links() {
+		$post_id = self::factory()->post->create();
+		add_syndication_link( $post_id, 'http://www.example.com' );		
+		add_syndication_link( $post_id, 'http://www2.example.com' );		
+		$get = get_syndication_links_data( $post_id );
+		$this->assertEquals( array( 'http://www.example.com', 'http://www2.example.com' ), $get );
+	}
+
+	public function test_replace_syndication_links() {
+		$post_id = self::factory()->post->create();
+		add_syndication_link( $post_id, 'http://www.example.com' );		
+		add_syndication_link( $post_id, 'http://www2.example.com', true );		
+		$get = get_syndication_links_data( $post_id );
+		$this->assertEquals( array( 'http://www2.example.com' ), $get );
+	}
+
+
+}
