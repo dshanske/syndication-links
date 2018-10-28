b393e70585a3f746b9725df45d7a45c65f605f09
David Shanske <david@shanske.com>
2018-10-27 21:25:32 -0500

    Update dependencies

--- tests/test-links.php
+++ tests/test-links.php
@@ -1,9 +1,9 @@
 <?php
 class SLinksTest extends WP_UnitTestCase {
-	 
+
 	public function test_set_and_retrieve_syndication_link() {
 		$post_id = self::factory()->post->create();
-		$link = 'http://www.example.com';
+		$link    = 'http://www.example.com';
 		add_syndication_link( $post_id, $link );
 		$get = get_syndication_links_data( $post_id );
 		$this->assertEquals( array( $link ), $get );
@@ -11,7 +11,7 @@ class SLinksTest extends WP_UnitTestCase {
 
 	public function test_set_and_retrieve_syndication_links() {
 		$post_id = self::factory()->post->create();
-		$link = array( 'http://www.example.com', 'http://www.example2.com' );
+		$link    = array( 'http://www.example.com', 'http://www.example2.com' );
 		add_syndication_link( $post_id, $link );
 		$get = get_syndication_links_data( $post_id );
 		$this->assertEquals( $link, $get );
@@ -19,16 +19,16 @@ class SLinksTest extends WP_UnitTestCase {
 
 	public function test_add_two_syndication_links() {
 		$post_id = self::factory()->post->create();
-		add_syndication_link( $post_id, 'http://www.example.com' );		
-		add_syndication_link( $post_id, 'http://www2.example.com' );		
+		add_syndication_link( $post_id, 'http://www.example.com' );
+		add_syndication_link( $post_id, 'http://www2.example.com' );
 		$get = get_syndication_links_data( $post_id );
 		$this->assertEquals( array( 'http://www.example.com', 'http://www2.example.com' ), $get );
 	}
 
 	public function test_replace_syndication_links() {
 		$post_id = self::factory()->post->create();
-		add_syndication_link( $post_id, 'http://www.example.com' );		
-		add_syndication_link( $post_id, 'http://www2.example.com', true );		
+		add_syndication_link( $post_id, 'http://www.example.com' );
+		add_syndication_link( $post_id, 'http://www2.example.com', true );
 		$get = get_syndication_links_data( $post_id );
 		$this->assertEquals( array( 'http://www2.example.com' ), $get );
 	}
