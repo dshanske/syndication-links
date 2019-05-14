5f0f92cc01e3536b99f84ddb9f676ea8f00457cd
David Shanske <david@shanske.com>
2019-05-14 11:14:22 +0000

    Fix styling

--- includes/class-syn-link-domain-icon-map.php
+++ includes/class-syn-link-domain-icon-map.php
@@ -1,29 +1,29 @@
 <?php
 class Syn_Link_Domain_Icon_Map {
 
-  private static $map = array(
-    'play.google.com'    => 'googleplay',
-    'plus.google.com'    => 'googleplus',
-    'indieweb.xyz'       => 'info',
-    'getpocket.com'      => 'pocket',
-    'flip.it'            => 'flipboard',
-    'micro.blog'         => 'microblog',
-    'wordpress.org'      => 'wordpress',
-    'itunes.apple.com'   => 'applemusic',
-  );
+	private static $map = array(
+		'play.google.com'  => 'googleplay',
+		'plus.google.com'  => 'googleplus',
+		'indieweb.xyz'     => 'info',
+		'getpocket.com'    => 'pocket',
+		'flip.it'          => 'flipboard',
+		'micro.blog'       => 'microblog',
+		'wordpress.org'    => 'wordpress',
+		'itunes.apple.com' => 'applemusic',
+	);
 
-  public static function getName($url) {
-    $parsed = parse_url($url);
-    if(false !== $parsed) {
-      $host = $parsed['host'];
-      if(array_key_exists($host,self::$map)) {
-        return self::$map[$host];
-      } else {
-        return false;
-      }
-    }
+	public static function getName( $url ) {
+		$parsed = parse_url( $url );
+		if ( false !== $parsed ) {
+			$host = $parsed['host'];
+			if ( array_key_exists( $host, self::$map ) ) {
+				return self::$map[ $host ];
+			} else {
+				return false;
+			}
+		}
 
-    return false;
-  }
+		return false;
+	}
 }
-?>
+
