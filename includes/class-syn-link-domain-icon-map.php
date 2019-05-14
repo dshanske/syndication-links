2ed5e4595295ee23fd148cceacabc196877580c5
Ruxton <ruxton@rhythmandpoetry.net>
2019-05-14 13:10:27 +0800

    Move special cases + overriding domain extract for them.
    How it ran at present, all google domains were set
    to name ‘google’ because domain extraction named them.

---
+++ includes/class-syn-link-domain-icon-map.php
@@ -0,0 +1,28 @@
+<?php
+class Syn_Link_Domain_Icon_Map {
+
+  private static $map {
+    'play.google.com'    => 'googleplay',
+    'plus.google.com'    => 'googleplus',
+    'indieweb.xyz'       => 'info',
+    'getpocket.com'      => 'pocket',
+    'flip.it'            => 'flipboard',
+    'micro.blog'         => 'microblog',
+    'wordpress.org'      => 'wordpress'
+  }
+
+  public static function getName($url) {
+    $parsed = parse_url($url)
+    if(false !=== $parsed) {
+      $host = $parsed['host'];
+      if(array_key_exists($host,$map)) {
+        return $map[$host];
+      } else {
+        return false;
+      }
+    }
+
+    return false;
+  }
+}
+?>
