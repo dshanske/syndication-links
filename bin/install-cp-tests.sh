15ed0bb344f5fe998e934c093e416837ee1ee811
David Shanske <david@shanske.com>
2024-11-13 05:48:04 +0000

    Fix again

--- bin/install-cp-tests.sh
+++ bin/install-cp-tests.sh
@@ -43,7 +43,7 @@ if [[ "$CP_VERSION" == latest ]]; then
 	download \
 		https://api.github.com/repos/ClassicPress/ClassicPress-release/releases/latest \
 		"$TMPDIR/cp-latest.json"
-	CP_VERSION=$(grep -o '"tag_name":"[^"]*' $TMPDIR/cp-latest.json | sed 's/"tag_name":"//')
+	CP_VERSION="$(grep -Po '"tag_name":\s*"[^"]+"' "$TMPDIR/cp-latest.json" | cut -d'"' -f4)"
 
 	if [ -z "$CP_VERSION" ]; then
 		echo "ClassicPress version not detected correctly!"
